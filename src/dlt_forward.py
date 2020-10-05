fe4ac133496a2968e350cc09a1e0515ed44b037f
yongzhi <yongzhi@MacBook-Pro.local>
2020-10-05 07:26:09 +0100

    Refactor forward propagation code. Add forward prop for basic RNN activation.

--- src/dlt_forward.py
+++ src/dlt_forward.py
@@ -1,72 +1,55 @@
 import torch
 from dlt_activate import sigmoid, tanh, softmax
 
-def forward_sigmoid(x, w, b):
-    ''' Forward propagate x, the input to this layer, with w and b, the weights in this layer, to obtain z. Activate z with the sigmoid function to produce a.
-
-    a = sigmoid (wx + b)
-
-    Args: 
-        x (matrix): A (n[0] x m) matrix, where n[0] is the number of nodes in the previous layer, and m is the number of examples.
-
-        w (matrix): A (n[1] x n[0]) matrix of weights in this layer, where n[1] is the number of nodes in this layer, 
-
-        b (matrix): A (n[1] x 1) matrix of constants in this layer, where n[1] is the number of nodes in this layer, 
-
-    Returns:
-        z: A (n[1] x m) matrix equals to wx + b.
-
-        matrix: A (n[1] x m) matrix where (every n[1] x 1) column is the activated forward propagation output for an example.
-    '''
-    z = torch.matmul(w, x) + b
-    return z, sigmoid(z)
-
-def forward_tanh(x, w, b):
-    ''' Forward propagate x, the input to this layer, with w and b, the weights in this layer, to obtain z. Activate z with the tanh function to produce a.
-
-    a = tanh (wx + b)
-
-    Args: 
-        x (matrix): A (n[0] x m) matrix, where n[0] is the number of nodes in the previous layer, and m is the number of examples.
-
-        w (matrix): A (n[1] x n[0]) matrix of weights in this layer, where n[1] is the number of nodes in this layer, 
-
-        b (matrix): A (n[1] x 1) matrix of constants in this layer, where n[1] is the number of nodes in this layer, 
-
-    Returns:
-        z: A (n[1] x m) matrix equals to wx + b.
-
-        matrix: A (n[1] x m) matrix where (every n[1] x 1) column is the activated forward propagation output for an example.
-    '''
-    z = torch.matmul(w, x.float()) + b
-    return z, tanh(z)
-
-def forward_softmax(x, w, b):
-    ''' Forward propagate x, the input to this layer, with w and b, the weights in this layer, to obtain z. Activate z with the softmax function to produce a.
-
-    a = softmax (wx + b)
-
-    Args: 
-        x (matrix): A (n[0] x m) matrix, where n[0] is the number of nodes in the previous layer, and m is the number of examples.
-
-        w (matrix): A (n[1] x n[0]) matrix of weights in this layer, where n[1] is the number of nodes in this layer, 
-
-        b (matrix): A (n[1] x 1) matrix of constants in this layer, where n[1] is the number of nodes in this layer, 
-
-    Returns:
-        z: A (n[1] x m) matrix equals to wx + b.
-
-        matrix: A (n[1] x m) matrix where (every n[1] x 1) column is the activated forward propagation output for an example.
-    '''
-    z = torch.matmul(w, x) + b
-    return z, softmax(z)
+def forward_activate(z, activationFunctionID):
+    if activationFunctionID == 'sigmoid':
+        return sigmoid(z)
+    elif activationFunctionID == 'tanh':
+        return tanh(z)
+    elif activationFunctionID == 'softmax':
+        return softmax(z)
+    else:
+        assert(False) # Unrecognized activation function ID string    
 
 def forward(x, w, b, activationFunctionID):
-    if activationFunctionID == 'sigmoid':
-        return forward_sigmoid(x, w, b)
-    elif activationFunctionID == 'tanh':
-        return forward_tanh(x, w, b)
-    elif activationFunctionID == 'softmax':
-        return forward_softmax(x, w, b)
-    else:
-        assert(False) # Unrecognized activation function ID string
+    ''' Forward propagate x, the input to this layer, with w and b, the weights in this layer, to obtain z. Activate z with the activation function to produce a.
+
+    a = activate (wx + b)
+
+    Args: 
+        x (matrix): A (n[0] x m) matrix, where n[0] is the number of nodes in the previous layer, and m is the number of examples.
+
+        w (matrix): A (n[1] x n[0]) matrix of weights in this layer, where n[1] is the number of nodes in this layer, 
+
+        b (matrix): A (n[1] x 1) matrix of constants in this layer, where n[1] is the number of nodes in this layer.
+
+    Returns:
+        z: A (n[1] x m) matrix equals to wx + b.
+
+        matrix: A (n[1] x m) matrix where (every n[1] x 1) column is the activated forward propagation output for an example.
+    '''    
+    z = torch.matmul(w, x.float()) + b
+    return z, forward_activate(z, activationFunctionID)
+
+
+def forward_rnn(x, aPrev, wa, ba, activationFunctionID):
+    ''' Forward propagate concatenated aPrev and x, with wa and ba, the weights in this cell, to obtain z. Activate z with the activation function to produce a.
+
+    Args:
+        x (matrix): A (nx x m) matrix, where nx is the number of input features, and m is the number of examples.
+
+        aPrev (matrix): A (na x m) matrix, where na is the number of activated features from the previous cell, and m is the number of examples.
+
+        wa (matrix): A (ni x (na+nx)) matrix, where ni is the number of nodes in this layer.
+
+        ba (matrix): A (ni x 1) matrix of constants in this layer, where ni is the number of nodes in this layer.
+
+    Returns:
+        z: A (ni x m) matrix, equals to wa.(aPrev, x) + ba, where (aPrev,x) is the concatenated aPrev and x.
+
+        matrix: A (ni x m) matrix, the result of running the activation function on z.
+    '''
+    z = torch.matmul(wa, torch.cat((aPrev, x), 0)) + ba
+    return z, forward_activate(z, activationFunctionID)    
+    
+    
--- src/dlt_forward_test.py
+++ src/dlt_forward_test.py
@@ -1,7 +1,8 @@
 import math
 import torch
 
-from dlt_forward import forward_sigmoid
+from dlt_activate import sigmoid, tanh, softmax
+from dlt_forward import forward, forward_rnn
 
 def test_forward_sigmoid():
     # x is a 4x3 matrix. 3 examples, 4 features or nodes per example.
@@ -14,7 +15,8 @@ def test_forward_sigmoid():
     br = torch.randn(2, 1)
 
     # a, the forward propagation result in the layer is a 2x3 matrix. 2 nodes or features for each of the 3 examples.
-    zr, ar = forward_sigmoid(xr, wr, br)
+    activationFunctionID = 'sigmoid'
+    zr, ar = forward(xr, wr, br, activationFunctionID)
 
     # check the values of ar against manual matrix multiplication.
     wr0 = wr[0,:]
@@ -39,4 +41,104 @@ def test_forward_sigmoid():
     assert math.isclose(ar[1][1], expected_value11, rel_tol=1e-05)
     
     assert math.isclose(ar[0][2], expected_value02, rel_tol=1e-05)
-    assert math.isclose(ar[1][2], expected_value12, rel_tol=1e-05)    
+    assert math.isclose(ar[1][2], expected_value12, rel_tol=1e-05)
+
+def test_forward_tanh():
+    # x is a 4x3 matrix. 3 examples, 4 features or nodes per example.
+    xr = torch.randn(4, 3)
+
+    # w is 2x4 matrix. 2 nodes in the layer being forward propagated. 4 nodes in the previous layer.
+    wr = torch.randn(2, 4)
+
+    # b is a 2x1 matrix, 2 constants for the 2 nodes in the layer being forward propagated.
+    br = torch.randn(2, 1)
+
+    # a, the forward propagation result in the layer is a 2x3 matrix. 2 nodes or features for each of the 3 examples.
+    activationFunctionID = 'tanh'
+    zr, ar = forward(xr, wr, br, activationFunctionID)
+
+    zExpected = torch.matmul(wr, xr) + br
+    aExpected = tanh(zExpected)
+
+    assert torch.allclose(zr, zExpected)
+    assert torch.allclose(ar, aExpected)
+
+def test_forward_softmax():
+    # x is a 4x3 matrix. 3 examples, 4 features or nodes per example.
+    xr = torch.randn(4, 3)
+
+    # w is 2x4 matrix. 2 nodes in the layer being forward propagated. 4 nodes in the previous layer.
+    wr = torch.randn(2, 4)
+
+    # b is a 2x1 matrix, 2 constants for the 2 nodes in the layer being forward propagated.
+    br = torch.randn(2, 1)
+
+    # a, the forward propagation result in the layer is a 2x3 matrix. 2 nodes or features for each of the 3 examples.
+    activationFunctionID = 'softmax'
+    zr, ar = forward(xr, wr, br, activationFunctionID)
+
+    zExpected = torch.matmul(wr, xr) + br
+    aExpected = softmax(zExpected)
+
+    assert torch.allclose(zr, zExpected)
+    assert torch.allclose(ar, aExpected)        
+
+
+def test_forward_rnn_sigmoid():
+    xr = torch.randn(4, 3)
+    aPrevr = torch.randn(2, 3)
+    war = torch.randn(3, 6)
+    bar = torch.randn(3, 1)
+
+    activationFunctionID = 'sigmoid'
+    z, a = forward_rnn(xr, aPrevr, war, bar, activationFunctionID)
+
+    xa = torch.cat((aPrevr, xr), 0)
+    assert xa.shape[0] == 6
+    assert xa.shape[1] == 3
+
+    zExpected = torch.matmul(war, xa) + bar
+    assert torch.allclose(z, zExpected)
+    
+    aExpected = sigmoid(zExpected)
+    assert torch.allclose(a, aExpected)
+
+def test_forward_rnn_tanh():
+    xr = torch.randn(4, 3)
+    aPrevr = torch.randn(2, 3)
+    war = torch.randn(3, 6)
+    bar = torch.randn(3, 1)
+
+    activationFunctionID = 'tanh'
+    z, a = forward_rnn(xr, aPrevr, war, bar, activationFunctionID)
+
+    xa = torch.cat((aPrevr, xr), 0)
+    assert xa.shape[0] == 6
+    assert xa.shape[1] == 3
+
+    zExpected = torch.matmul(war, xa) + bar
+    assert torch.allclose(z, zExpected)
+    
+    aExpected = tanh(zExpected)
+    assert torch.allclose(a, aExpected)
+
+def test_forward_rnn_softmax():
+    xr = torch.randn(4, 3)
+    aPrevr = torch.randn(2, 3)
+    war = torch.randn(3, 6)
+    bar = torch.randn(3, 1)
+
+    activationFunctionID = 'softmax'
+    z, a = forward_rnn(xr, aPrevr, war, bar, activationFunctionID)
+
+    xa = torch.cat((aPrevr, xr), 0)
+    assert xa.shape[0] == 6
+    assert xa.shape[1] == 3
+
+    zExpected = torch.matmul(war, xa) + bar
+    assert torch.allclose(z, zExpected)
+    
+    aExpected = softmax(zExpected)
+    assert torch.allclose(a, aExpected)        
+    
+
